239f801896f22ba6dced8276cce2d7b730b39375
Havis Dan <danmole.havis@gmail.com>
2023-08-08 12:50:00 -0700

    first commit

---
+++ src/components/TvCard.jsx
@@ -0,0 +1,26 @@
+import{Link} from 'react-router-dom'
+import {Image} from 'react-bootstrap'
+
+export default function TvCard({data}) {
+    console.log('Tvcard', data)
+  return (
+    <Link to={`/tvshows/${data.id}`}>
+        <Image src={data?.image?.original}
+        className='tvcard'/>
+        <p className='text-black fs-5'>{data.name}</p>
+    
+    </Link>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
